597d032f7a9cf55d9191a0051411fd57254b59a4
Stephen Schuez <stephen.schuez@gmail.com>
2020-02-28 15:08:28 +0100

    first draft of show pager per user and added some login logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+	def age?
+		((Time.zone.now - self.date_of_birth.to_time) / 1.year.seconds).floor         	
+	end
+
+	def member_since_months
+		months = (Date.today.year - self.started.year) * 12 + Date.today.month - self.started.month - (Date.today.day >= self.started.day ? 0 : 1)
+	end         
+
+	def date_diff(date1,date2)
+	  month = (date2.year * 12 + date2.month) - (date1.year * 12 + date1.month)
+	  month_array = month.divmod(12)
+	  if month_array[0] > 0
+	  	"#{month_array[0]}y #{month_array[1]}m"
+	  else
+	  	"#{month_array[1]}m"
+	  end
+	end
+
+
+
 end

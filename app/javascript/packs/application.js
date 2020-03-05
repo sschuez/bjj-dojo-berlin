9e7e42273f247f6bcbde62c0e55e4bb568bc095a
Stephen Schuez <stephen.schuez@gmail.com>
2020-03-05 22:53:55 +0100

    adding comments to profile page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("packs/switch")
 
 import "bootstrap";
+
---
+++ app/javascript/packs/switch.js
@@ -0,0 +1,10 @@
+console.log('Hello there');
+
+const mySwitch = document.getElementById('customSwitch1');
+
+mySwitch.addEventListener("click", (event) => {
+	console.log(event);
+	event.currentTarget.classList.toggle("active");
+});
+
+console.log(mySwitch);
